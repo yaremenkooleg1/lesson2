17a2155f32193245a2a0bb77e0fa9ac291533239
Oleg Yaremenko <yaremenkooleg1@gmail.com>
2016-12-08 21:03:38 +0200

    seccond commit

---
+++ out/com/hillel/variables/Vars.java
@@ -0,0 +1,11 @@
+package com.hillel.variables;
+
+public class Vars {
+public static void public static void main(String[] args) {
+
+}
+  byte var= 1;
+  var=var+128;
+  System.out.println(var);
+var=var+128;
+}
---
+++ src/com/hillel/variables/Vars.java
@@ -0,0 +1,28 @@
+package com.hillel.variables;
+
+public class Vars {
+public static void main(String[] args) {
+
+
+//  byte var = 127;
+  // int num = var + 1;
+//  byte anotherVar = (byte) num;
+
+/*
+
+another comment
+
+*/
+
+  float var = 3.5f;
+  double d = 34.45;
+  double a =30;
+ double b =25;
+  double c = a/b;
+
+ System.out.println(c);
+ System.out.println(hello);
+
+
+}
+}
